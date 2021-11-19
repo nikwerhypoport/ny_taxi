3c62609956a84ba8583e4ae3dda61379d80c5686
Nikolaus.Werner <nikolaus.werner@europace.de>
2021-11-19 14:14:02 +0100

    letzte Änderungen

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ny_taxi
+
+## install
+
+- requires pipenv and a python version stated in [Pipfile](Pipfile)
+- `pipenv install`
+
+
+## run preprocessor
+Small script for reducing size of data and enriching necessary columns for the next steps.
+- requires taxi csv [data](https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page) in location [resources/data](resources/data) togehter with lookup file `taxi+_zone_lookup.csv`
+- `pipenv shell`
+- in the shell, run `pipenv run preprocess` 
+
+## notebook
+- just for testing 
+
+
--- etl/preprocessor.py
+++ etl/preprocessor.py
@@ -11,11 +11,11 @@ COLUMNS_OF_INTEREST = {
     "PULocationID",
     "DOLocationID",
 }
-COLUMN_MAPPINGS = {}
 
+DATA_DIR = "resources/data/"
 
 def preprocess(lookup_table: pd.DataFrame) -> None:
-    for source_file_name in glob.glob("../resources/data/yellow*.csv"):
+    for source_file_name in glob.glob(f"{DATA_DIR}/*tripdata*.csv"):
         df: pd.DataFrame = pd.read_csv(source_file_name)
         df = _transform(df, lookup_table)
         target_file_name = f"{source_file_name}.parquet"
@@ -23,10 +23,22 @@ def preprocess(lookup_table: pd.DataFrame) -> None:
 
 
 def _transform(df: pd.DataFrame, lookup_table: pd.DataFrame) -> pd.DataFrame:
-    df: pd.DataFrame = _drop_unneccessary_cols(df)
-    enriched_df: pd.DataFrame = _enrich_pickup_location(df, lookup_table)
+    enriched_df: pd.DataFrame = _drop_cols(df=df, cols_to_keep=COLUMNS_OF_INTEREST)
+    enriched_df = _enrich_locations(enriched_df, lookup_table)
+    enriched_df = _extract_year_month(enriched_df)
+    return enriched_df
+
+def _extract_year_month(df: pd.DataFrame) -> pd.DataFrame:
+    df['year'] = df['tpep_dropoff_datetime'].str.extract("^(\d+).*")
+    df['month'] = df['tpep_dropoff_datetime'].str.extract("^\d+-(\d+).*")
+    df = df.assign(month=df.year + "-" + df.month)
+    df = df.drop(columns=["year"], axis=1)
+    return df
+
+def _enrich_locations(df: pd.DataFrame, lookup_table: pd.DataFrame) -> pd.DataFrame:
+    enriched_df = _enrich_pickup_location(df, lookup_table)
     enriched_df = _enrich_drop_off_location(enriched_df, lookup_table)
-    enriched_df = enriched_df.drop(columns=["LocationID_y", "LocationID_x"])
+    enriched_df = enriched_df.drop(columns=["LocationID_y", "LocationID_x"], axis=1)
     return enriched_df
 
 
@@ -52,19 +64,19 @@ def _enrich_pickup_location(
     return enriched_df
 
 
-def _drop_unneccessary_cols(df) -> pd.DataFrame:
-    columns_to_drop: Set = _get_columns_to_drop(set(df.columns))
+def _drop_cols(df: pd.DataFrame, cols_to_keep: Set) -> pd.DataFrame:
+    columns_to_drop: Set = _get_columns_to_drop(set(df.columns), cols_to_keep)
     df = df.drop(columns_to_drop, axis=1)
     return df
 
 
-def _get_columns_to_drop(columns: Set) -> Set:
-    diff = COLUMNS_OF_INTEREST.symmetric_difference(columns)
+def _get_columns_to_drop(columns: Set, cols_to_keep: Set) -> Set:
+    diff = cols_to_keep.symmetric_difference(columns)
     return diff
 
 
 def read_lookup_table() -> pd.DataFrame:
-    return pd.read_csv("../resources/data/taxi+_zone_lookup.csv")
+    return pd.read_csv(f"{DATA_DIR}/taxi+_zone_lookup.csv")
 
 
 if __name__ == "__main__":
@@ -72,8 +84,10 @@ if __name__ == "__main__":
     Small helper script for making life easier when dealing with huge csv files which need to be read constantly
     - to parquet for speed
     - drop unnecessary columns for the task
-
+    - enrich necessary columns for the task
+    - writes target files to directory where files are read from
     This would be integrated as a preprocessing step in a production system
     """
+
     lookup_df = read_lookup_table()
     preprocess(lookup_df)
